e0ba9b9868c4759d0584930edca0e9444018f49c
stanforddesignprogram <stanforddesignprogram@gmail.com>
2017-06-09 18:32:11 -0700

    Added color changing and clone (issues with anchor and running on device)

--- Assets/Scripts/GlowScriptLocal.cs
+++ Assets/Scripts/GlowScriptLocal.cs
@@ -1,36 +1,87 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using HoloToolkit.Unity.SpatialMapping;
 using UnityEngine;
 
 public class GlowScriptLocal : MonoBehaviour {
+    //protected HoloToolkit.Unity.WorldAnchorManager anchorManager; // ???
+
     private SphereCollider m_sc;
     private Light m_light;
+    private TapToPlace m_ttp;
+    private float DefaultIntensity = 1.4f;
     private float MinLightRange = 0.0f;
     private float MaxLightRange = 0.3f;
-    private float LightSmoothTime = 0.1f;
+    private float GlobalSmoothTime = 0.1f;
+    private float GlobalDeltaPerChange = 0.1f;
+    private string GlowTag;
+
+    private Color HighColor = Color.green;
+    private Color LowColor = Color.blue;
+    private Color PrivateColor = Color.white;
 
     [SerializeField]
-    private float targetLightRange = 0.1f; // Default
+    private float targetLightRange = 0.1f; // Starting value
     private float lightRangeDelta;
+    private float currColorScale = 1.0f; // Starting value, from 0 to 1
+    private Color currColor;
+    private float targetColorScale;
 
     // Use this for initialization
     void Start () {
+        GlowTag = gameObject.tag;
+        m_ttp = gameObject.GetComponent<TapToPlace>();
         m_sc = GetComponent<SphereCollider>();
         m_light = GetComponent<Light>();
         m_light.range = targetLightRange;
-        lightRangeDelta = (MaxLightRange - MinLightRange) / 10.0f; // Default each press moves 10% of range
+        m_light.intensity = DefaultIntensity;
+        m_light.color = GlowTag == "AllPublic" ? HighColor : PrivateColor;
+        currColor = m_light.color;
+        targetColorScale = currColorScale;
+        lightRangeDelta = (MaxLightRange - MinLightRange) * GlobalDeltaPerChange; // Default each press moves 10% of range
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-            targetLightRange = Mathf.Min(MaxLightRange, m_light.range + lightRangeDelta);
 
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-            targetLightRange = Mathf.Max(MinLightRange, m_light.range - lightRangeDelta);
+    // Update is called once per frame
+    void Update() {
+        // Only listen to input if this object is being placed
+        if (!m_ttp.IsBeingPlaced) return;
 
-        float v = 0.0f;
-        m_light.range = Mathf.SmoothDamp(m_light.range, targetLightRange , ref v, LightSmoothTime);
-        m_sc.radius = m_light.range / 2.0f;
+        // Clones the current object
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            GameObject glowClone = Instantiate(gameObject);
+            m_ttp.SavedAnchorFriendlyName = "GlowClone_" + System.DateTime.Now.ToString("yyyyMMddhhmmss");
+            glowClone.GetComponent<TapToPlace>().IsBeingPlaced = false;
+            HoloToolkit.Unity.WorldAnchorManager.Instance.AttachAnchor(glowClone, m_ttp.SavedAnchorFriendlyName); /// ???
+        }
+
+        if (GlowTag != "AllPrivate")
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow)) targetLightRange = Mathf.Min(MaxLightRange, m_light.range + lightRangeDelta);
+            if (Input.GetKeyDown(KeyCode.DownArrow)) targetLightRange = Mathf.Max(MinLightRange, m_light.range - lightRangeDelta);
+
+            float v1 = 0.0f;
+            m_light.range = Mathf.SmoothDamp(m_light.range, targetLightRange, ref v1, GlobalSmoothTime);
+            m_sc.radius = m_light.range / 2.0f;
+        }
+
+        if (GlowTag == "AllPublic")
+        {
+            if (Input.GetKeyDown(KeyCode.RightArrow)) targetColorScale = Mathf.Min(1, currColorScale + GlobalDeltaPerChange);
+            if (Input.GetKeyDown(KeyCode.LeftArrow)) targetColorScale = Mathf.Max(0, currColorScale - GlobalDeltaPerChange);
+
+            float v2 = 0.0f;
+            currColorScale = Mathf.SmoothDamp(currColorScale, targetColorScale, ref v2, GlobalSmoothTime);
+            float relativeScale = currColorScale;
+            if (currColorScale > 0.5f)
+            {
+                relativeScale = (1.0f - relativeScale) / 0.5f;
+                currColor.b = relativeScale;
+            }
+            else
+            {
+                relativeScale /= 0.5f;
+                currColor.g = relativeScale;
+            }
+            m_light.color = currColor;
+        }
 	}
 }
