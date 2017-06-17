7ccae83aca1c8f43a9a9c95e3f15afc66f90e541
stanforddesignprogram <stanforddesignprogram@gmail.com>
2017-06-17 02:55:39 -0700

    Added lots of comments

--- Assets/Scripts/GlowScriptLocal.cs
+++ Assets/Scripts/GlowScriptLocal.cs
@@ -6,24 +6,24 @@ public class GlowScriptLocal : MonoBehaviour {
     private SphereCollider m_sc;
     private Light m_light;
     private TapToPlace m_ttp;
-    private float DefaultIntensity = 1.4f;
-    private float MinLightRange = 0.0f;
-    private float MaxLightRange = 0.3f;
+    private float DefaultIntensity = 1.4f; // Seems like the light halos (glows) max out at 1.4 intensity
+    private float MinLightRange = 0.0f; // Minimum size of halo
+    private float MaxLightRange = 0.3f; // Maximunm size of halo
     private float GlobalSmoothTime = 0.1f;
-    private float GlobalDeltaPerChange = 0.1f;
-    private string GlowGroup;
-    private string GlowTag;
+    private float GlobalDeltaPerChange = 0.1f; // 0.1f means 10% change for every keypress, both for size and color
+    private string GlowGroup; // All glows in a group respond together to the same keypresses (it's set to be the tag of the parent object)
+    private string GlowTag; // Can be "AllPublic", "ValencePrivate", or "AllPrivate"
 
-    private Color HighColor = Color.green;
+    private Color HighColor = Color.green; // Default high valence color, is also default starting color for "AllPublic" glows
     //private Color LowColor = Color.red;
-    private Color PrivateColor = Color.white;
+    private Color PrivateColor = Color.white; // Default color for glows that DON'T show valence, so currently both the "ValencePrivate" and "AllPrivate" glows
 
     [SerializeField]
-    private float targetLightRange = 0.1f; // Starting value
-    private float lightRangeDelta;
-    private float currColorScale = 1.0f; // Starting value, from 0 to 1
-    private Color currColor;
-    private float targetColorScale;
+    private float targetLightRange = 0.1f; // Keeps track of how large (or small) the light range is smoothly trying to get (value here specifies starting value)
+    private float lightRangeDelta; // Will be set to whatever value is X% between the min and max light ranges (see Start() below)
+    private float currColorScale = 1.0f; // A value from 0 to 1 that represents current valence
+    private Color currColor; // A Color that represents the current color
+    private float targetColorScale; // A value from 0 to 1 that represents valence that we're trying to smoothly arrive at
 
     // Use this for initialization
     void Start () {
@@ -74,33 +74,42 @@ public class GlowScriptLocal : MonoBehaviour {
     {
         if (GlowTag != "AllPrivate")
         {
+            // Sets targetLightRange to be X% higher or lower than the current light range, bounded by max and min
             if (Input.GetKeyDown(upArousal)) targetLightRange = Mathf.Min(MaxLightRange, m_light.range + lightRangeDelta);
             if (Input.GetKeyDown(downArousal)) targetLightRange = Mathf.Max(MinLightRange, m_light.range - lightRangeDelta);
 
+            // Smoothly change light range toward the targetLightRange
             float v1 = 0.0f;
             m_light.range = Mathf.SmoothDamp(m_light.range, targetLightRange, ref v1, GlobalSmoothTime);
+
+            // Keep the SphereCollider radius of the halo (glow) half of the range
             m_sc.radius = m_light.range / 2.0f;
         }
 
         if (GlowTag == "AllPublic")
         {
+            // Sets targetColorScale to be X% higher or lower than the current color scale, bounded by max and min
             if (Input.GetKeyDown(upValence)) targetColorScale = Mathf.Min(1, currColorScale + GlobalDeltaPerChange);
             if (Input.GetKeyDown(downValence)) targetColorScale = Mathf.Max(0, currColorScale - GlobalDeltaPerChange);
 
+            // Smoothly change the color scale toward the targetColorScale
             float v2 = 0.0f;
             currColorScale = Mathf.SmoothDamp(currColorScale, targetColorScale, ref v2, GlobalSmoothTime);
 
+            // Translate color scale (basically valence value from 0 to 1) into an actual color
             float relativeScale = currColorScale;
-            if (currColorScale > 0.5f)
+            if (currColorScale > 0.5f) // Assuming red to green (0 to 1), as color scale moves over 0.5, we inversely scale back red value
             {
-                relativeScale = (1.0f - relativeScale) / 0.5f;
+                relativeScale = (1.0f - relativeScale) / 0.5f; // Assuming red to green, 1.0 color scale should mean 0.0 red, and anything less than 0.5 scale should mean 1.0 red
                 currColor.r = relativeScale;
             }
-            else
+            else // Assuming red to green (0 to 1), as color scale moves under 0.5, we linearly scale back green value
             {
-                relativeScale /= 0.5f;
+                relativeScale /= 0.5f; // Assuming red to green, 0.0 color scale should mean 0.0 green, and anything 0.5 and above should mean 1.0 green
                 currColor.g = relativeScale;
             }
+
+            // Set the light color to the current color that was updated above
             m_light.color = currColor;
         }
     }
